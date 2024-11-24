44a8b9a532bdb37b87bc99e45a454c4ae2f1a8c9
attrams <48452451+Attrams@users.noreply.github.com>
2024-11-24 21:14:04 +0000

    feat: add WorkforceHubAPI.Entities class library with Company and Employee models.
    - Created WorkforceHubAPI.Entities project to define domain models for the application.
    - Added a Models folder containing:
        - `Company` model representing a company entity with properties such as Id, Name, Address, Country, and Employees.
        - `Employee` model representing an employee entity with properties such as Id, Name, Age, Position, and associated Company.
    - Configured the class library as part of the main solution.
    - Included necessary attributes (e.g., [Required], [MaxLength], [ForeignKey]) to enforce validation and relationships.

---
+++ WorkforceHubAPI.Entities/Models/Company.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkforceHubAPI.Entities.Models;
+
+/// <summary>
+/// Represents a company entity in the application.
+/// </summary>
+public class Company
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the company.
+    /// </summary>
+    [Column("CompanyId")]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the company.
+    /// </summary>
+    /// <remarks>
+    /// This field is required and has a maximum length of 60 characters.
+    /// </remarks>
+    [Required(ErrorMessage = "Company name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the address of the company.
+    /// </summary>
+    /// <remarks>
+    /// This field is required and has a maximum length of 60 characters.
+    /// </remarks>
+    [Required(ErrorMessage = "Company address is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Address is 60 characters.")]
+    public string? Address { get; set; }
+
+    /// <summary>
+    /// Gets or sets the country where the company is located.
+    /// </summary>
+    public string? Country { get; set; }
+
+    /// <summary>
+    /// Navigation property for the collection of employees assiciated with the company.
+    /// </summary>
+    public ICollection<Employee>? Employees { get; set; }
+}
---
+++ WorkforceHubAPI.Entities/Models/Employee.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WorkforceHubAPI.Entities.Models;
+
+
+/// <summary>
+/// Represents an employee entity in the application.
+/// </summary>
+public class Employee
+{
+    /// <summary>
+    /// Gets or sets the unique identifier for the employee.
+    /// </summary>
+    [Column("EmployeeId")]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the name of the employee.
+    /// </summary>
+    /// <remarks>
+    /// This field is required and has a maximum length of 30 characters.
+    /// </remarks>
+    [Required(ErrorMessage = "Employee name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the age of the employee.
+    /// </summary>
+    /// <remarks>
+    /// This field is required.
+    /// </remarks>
+    [Required(ErrorMessage = "Age is a required field.")]
+    public int Age { get; set; }
+
+    /// <summary>
+    /// Gets or sets the position of the employee within the company.
+    /// </summary>
+    /// <remarks>
+    /// This field is required and has a maximum length of 20 characters.
+    /// </remarks>
+    [Required(ErrorMessage = "Position is a required field.")]
+    [MaxLength(20, ErrorMessage = "Maximum length for the position is 20")]
+    public string? Position { get; set; }
+
+    /// <summary>
+    /// Gets or sets the identifier of the company the employee is associated with.
+    /// </summary>
+    [ForeignKey(nameof(Company))]
+    public Guid CompanyId { get; set; }
+
+    /// <summary>
+    /// Navigation property for the associated company.
+    /// </summary>
+    public Company? Company { get; set; }
+}
